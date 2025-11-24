a907152ef58b977ccfdc5cb987d44d24401151d7
Timurid <kklounov702@gmail.com>
2025-11-24 17:53:17 +0500

    Импорт
    Импортировал необходимые библиотеки

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
